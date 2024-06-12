19ac809e78fc414b90c8529ec9be2cae61fb0231
poppdev1 <141820132+poppdev1@users.noreply.github.com>
2024-06-12 17:06:00 +0800

    Update ERC6551Account.sol

--- ERC6551Account.sol
+++ ERC6551Account.sol
@@ -2,13 +2,16 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 import '@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol';
+import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import {IERC6551Account} from '../../interfaces/IERC6551Account.sol';
 import {IERC6551Executable} from '../../interfaces/IERC6551Executable.sol';
 
-contract ERC6551Account is IERC165, IERC1271, IERC6551Account, IERC6551Executable, IERC1155Receiver {
+contract ERC6551Account is IERC165, IERC1271, IERC6551Account, IERC6551Executable, IERC1155Receiver, IERC721Receiver {
     uint256 public state;
 
     receive() external payable {}
@@ -84,6 +87,15 @@ contract ERC6551Account is IERC165, IERC1271, IERC6551Account, IERC6551Executabl
         return signer == owner();
     }
 
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4){
+        return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
+    }
+
     function onERC1155Received(
         address operator,
         address from,
@@ -102,5 +114,36 @@ contract ERC6551Account is IERC165, IERC1271, IERC6551Account, IERC6551Executabl
         bytes calldata data
     ) external override returns (bytes4){
         return bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
+    }
+
+    function batchTransferERC20(address[] calldata erc20Addresses, address payable to) public {
+        require(_isValidSigner(msg.sender), "Invalid signer");
+        for(uint i=0;i<erc20Addresses.length;i++){
+            address erc20Address = erc20Addresses[i];
+            if (erc20Address == address(0)){
+                to.send(address(this).balance);
+            } else {
+                IERC20(erc20Address).transfer(to, IERC20(erc20Address).balanceOf(address(this)));
+            }
+        }
+    }
+
+    function batchTransferERC721(address[] calldata erc721Addresses, uint256[] calldata ids, address to) public {
+        require(_isValidSigner(msg.sender), "Invalid signer");
+        require(erc721Addresses.length == ids.length, 'Invalid params');
+        for(uint i=0;i<erc721Addresses.length;i++){
+            address erc721Address = erc721Addresses[i];
+            IERC721(erc721Address).transferFrom(address(this), to, ids[i]);
+        }
+    }
+
+    function batchTransferERC1155(address[] calldata erc1155Addresses, uint256[] calldata ids, address to) public {
+        require(_isValidSigner(msg.sender), "Invalid signer");
+        require(erc1155Addresses.length == ids.length, 'Invalid params');
+        for(uint i=0;i<erc1155Addresses.length;i++){
+            address erc1155Address = erc1155Addresses[i];
+            uint256 amount = IERC1155(erc1155Address).balanceOf(address(this), ids[i]);
+            IERC1155(erc1155Address).safeTransferFrom(address(this), to, ids[i], amount, '0x');
+        }
     }
 }
